f97200ecb0c38047be9fbbec014599418d9e2a5c
silentgolem <liamlogan16@yahoo.co.uk>
2017-08-17 18:19:15 +0100

    silentgolem [6:19 PM] Added the facilities and care types to the edit details form. They also auto-fill

--- NursingHomesFinal/src/app/edit-details/edit-details.component.ts
+++ NursingHomesFinal/src/app/edit-details/edit-details.component.ts
@@ -12,6 +12,35 @@ import { User } from "../User";
 export class EditDetailsComponent implements OnInit {
   currentHome: Home;
   currentUser: User;
+  facilities: any[] = [
+    { "id": 0, "name": "Live-In Carers", "value": false },
+    { "id": 1, "name": "Chiropody", "value": false },
+    { "id": 2, "name": "Oratory", "value": false },
+    { "id": 3, "name": "Visiting Area", "value": false },
+    { "id": 4, "name": "Hairdressing", "value": false },
+    { "id": 5, "name": "Laundry", "value": false },
+    { "id": 6, "name": "Library", "value": false },
+    { "id": 7, "name": "WiFi", "value": false },
+    { "id": 8, "name": "Resident GP", "value": false },
+    { "id": 9, "name": "Dietician", "value": false },
+    { "id": 10, "name": "Dental Care", "value": false },
+    { "id": 11, "name": "Pool", "value": false },
+    { "id": 12, "name": "Garden", "value": false },
+    { "id": 13, "name": "Group Outings", "value": false },
+    { "id": 14, "name": "Bingo", "value": false }
+  ];
+  careTypes: any[] = [
+    { "id": 0, "name": "Alzheimer’s", "value": false },
+    { "id": 1, "name": "Cancer", "value": false },
+    { "id": 2, "name": "Hearing", "value": false },
+    { "id": 3, "name": "Speech Area", "value": false },
+    { "id": 4, "name": "Visual", "value": false },
+    { "id": 5, "name": "Residential", "value": false },
+    { "id": 6, "name": "Respite", "value": false },
+    { "id": 7, "name": "Convalescent", "value": false },
+    { "id": 8, "name": "Dementia", "value": false },
+    { "id": 9, "name": "Physiotherapy", "value": false }
+  ];
 
   constructor(private storageService: StorageService, private router: Router) {
     this.GetCurrentHome();
@@ -24,61 +53,63 @@ export class EditDetailsComponent implements OnInit {
   GetCurrentHome(): void {
     this.currentHome = this.storageService.getCurrentHome();
   }
-  CheckHome():boolean {
+  CheckHome(): boolean {
     if (this.currentHome != null || this.currentHome != undefined) {
-      if(this.currentHome.userID == this.currentUser.email) return true;
+      this.PopulateFacilities();
+      if (this.currentHome.userID == this.currentUser.email) return true;
       else this.Redirect();
     }
     else {
       return false;
     }
   }
+  PopulateFacilities(): void {
+    this.facilities.forEach(element => {
+      element.value=this.currentHome.facilities[element.id];
+    });
+    this.careTypes.forEach(element => {
+      element.value=this.currentHome.careTypes[element.id];
+    });
+  }
   Redirect(): void {
     this.router.navigateByUrl('/home');
   }
-  GetName():string
-  {
+  GetName(): string {
     return this.currentHome.name;
   }
-  GetAddress():string
-  {
+  GetAddress(): string {
     return this.currentHome.address;
   }
-  GetCounty():string
-  {
+  GetCounty(): string {
     return this.currentHome.county;
   }
-  GetCountry():string
-  {
+  GetCountry(): string {
     return this.currentHome.country;
   }
-  GetPhone():string
-  {
+  GetPhone(): string {
     return this.currentHome.phone;
   }
-  GetEmail():string
-  {
+  GetEmail(): string {
     return this.currentHome.email;
   }
-  GetContact():string
-  {
+  GetContact(): string {
     return this.currentHome.contact;
   }
-  GetSite():string
-  {
+  GetSite(): string {
     return this.currentHome.site;
   }
-  GetBeds():string
-  {
+  GetBeds(): string {
     return this.currentHome.beds;
   }
-  GetStaff():string
-  {
+  GetStaff(): string {
     return this.currentHome.staff;
   }
-  GetDescription():string
-  {
+  GetDescription(): string {
     return this.currentHome.description;
+  }
+  CheckID(test): boolean {
+    if (test%5==0&&test!=0) return true
+    else return false
   }
   ngOnInit() {
   }

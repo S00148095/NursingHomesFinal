366f864d85d7013ed6492d7455d8172c09ac9921
Mark Molloy <markmolloy57@gmail.com>
2018-01-29 20:25:53 +0000

    fixed mobile menu

--- NursingHomesFinal/src/hamburger.js
+++ NursingHomesFinal/src/hamburger.js
@@ -1,19 +1,35 @@
+
 $( document ).ready(function() {
 
+    var menuDisplayed = false;
     $( ".cross" ).hide();
     $( ".menu" ).hide();
     $( ".hamburger" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".hamburger" ).hide();
-    $( ".cross" ).show();
-    });
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+            menuDisplayed = true;
+        });
     });
     
     $( ".cross" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+            menuDisplayed = false;
+        });
     });
+
+    
+
+    $( document ).click(function(){
+        //console.log('clicked, and menuD = ' + menuDisplayed);
+        if (menuDisplayed){
+            $( ".cross" ).hide();
+            $( ".menu" ).hide();
+            $( ".hamburger" ).show();
+            menuDisplayed = false;
+        }
     });
     
 });//end doc.ready
